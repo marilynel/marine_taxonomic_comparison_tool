02e5bb1c6db3b3d20aaed57bb926b9dba9e9d116
Marilyn Leary <marilyneleary@gmail.com>
2021-10-22 20:31:05 -0700

    Update butts.py

--- butts.py
+++ butts.py
@@ -9,6 +9,8 @@ main_window = Tk()
 Label(main_window, text = "Genus:").grid(row = 0, column = 0)
 Label(main_window, text = "species:").grid(row = 1, column = 0)
 
+outputText = "ok"
+
 # text input
 genus = Entry(main_window, width = 50, borderwidth = 5)
 genus.grid(row = 0, column = 1)
@@ -17,6 +19,7 @@ spp.grid(row = 1, column = 1)
 
 #callback function
 def on_click():
+    global outputText
     name = genus.get() + " " + spp.get()
     #print(f"my name is {genus.get()} and my age is {spp.get()}")
     safe_string = urllib.parse.quote_plus(name)
@@ -24,11 +27,19 @@ def on_click():
     url = url.format(safe_string)
     response = requests.get(url)
     if response:
-        print(response.json())
+        #print(response.json())
+        orgData = response.json()
+        for i in orgData[0]:
+            if i == 'phylum':
+                #print(f"The phylum for {name} is {orgData[0][i]}")
+                outputText = f"The phylum for {name} is {orgData[0][i]}"
+        textLabel.config(text = outputText)
 
 
 # button widget
-Button(main_window, text = "Click Me", command = on_click ).grid(row = 2, column = 1)
-
+Button(main_window, text = "Search", command = on_click ).grid(row = 2, column = 1)
+textLabel = Label(main_window, text = "")
+textLabel.grid(row = 3, columnspan = 2)
 
 main_window.mainloop()
+
